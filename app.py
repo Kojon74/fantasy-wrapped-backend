c0bebc31001186e3617a60acf46cdcdba0f29693
Ken Johnson <dragonkenkj@gmail.com>
2025-02-24 00:50:08 -0800

    added biggest bust/steal

--- app.py
+++ app.py
@@ -14,12 +14,29 @@ def get_fantasy_data():
     token = {"access_token": access_token, "refresh_token": refresh_token}
     query = Query(token, league_key)
     teams, standings = query.get_teams()
+    alternative_reality_matrix, team_order = query.get_alternative_realities()
+    draft_busts, draft_steals = query.get_draft_busts_steals()
     metrics = [
         {
             "title": '"Official" Results',
             "description": "Sure, these are the official results. But were they really the best team? The luckiest? The biggest flop? Keep scrolling to uncover the real winners and losers of the season.",
             "stats": standings
-        }
+        },
+        {
+            "title": 'Alternative Realities',
+            "description": "What if your team had a different schedule? This matrix reimagines the season by swapping team schedules, showing how records would have changed in an alternate universe. Did bad luck hold you back, or were you truly dominant no matter the matchups?",
+            "stats": {"alternative_reality_matrix": alternative_reality_matrix, "team_order": team_order}
+        },
+        {
+            "title": 'Draft Steal',
+            "description": "Some picks turn out to be absolute gems! This metric highlights the player who delivered the biggest return on investment, massively outperforming their draft position. Whether it was a late-round sleeper who dominated or a mid-round pick who played like a first-rounder, this is your league's ultimate steal of the draft.",
+            "stats": draft_steals
+        },
+        {
+            "title": 'Draft Bust',
+            "description": "Not all picks live up to the hype. This metric identifies the player who fell the hardest from expectations, drastically underperforming their draft position. Whether it was due to injuries, poor form, or just bad luck, this was the pick that stung the most for fantasy managers.",
+            "stats": draft_busts
+        },
     ]
     response = {
         "teams": teams,
--- query.py
+++ query.py
@@ -3,6 +3,8 @@ from yahoo_oauth import OAuth2
 import requests
 from requests.adapters import HTTPAdapter, Retry
 from utils import xml_to_dict
+from collections import defaultdict
+
 from yahoo_oauth_wrapper import YahooOAuthWrapper
 
 BASE_URL = "https://fantasysports.yahooapis.com/fantasy/v2"
@@ -13,8 +15,10 @@ class Query():
         
         self.oauth.refresh_access_token() # In case access_token is already expired
         self.session = self.init_session()
+
         self.league_key = league_key
         self.game_id, _, self.league_id = league_key.split(".")
+        self.get_league()
 
 
     def authenticate(self, token):
@@ -49,13 +53,96 @@ class Query():
         data = xml_to_dict(response.text)
         return data
 
+    def get_league(self):
+        url = f"{BASE_URL}/league/{self.league_key};out=standings,settings"
+        league = self.get_response(url)["league"]
+        self.league_start_week = int(league["start_week"])
+        self.league_end_week = int(league["end_week"])
+        self.playoff_start_week = int(league["settings"].get("playoff_start_week", None))
+        self.teams = league["standings"]["teams"]
+
     def get_teams(self):
         """
         Returns:
         {[team_key: string]: {team_image: string, team_name: string, team_nickname: string}}
         """
-        url = f"{BASE_URL}/league/{self.league_key};out=standings"
-        teams = self.get_response(url)["league"]["standings"]["teams"]
-        teams_dict = {team["team_key"]: {"image": team["managers"]["manager"]["image_url"], "name": team["name"], "nickname": team["managers"]["manager"]["nickname"]} for team in teams}
-        standings = [{"key": team["team_key"], "value": i+1} for i, team in enumerate(teams)] # More efficient to combine two loops?
+        teams_dict = {team["team_key"]: {"image": team["managers"]["manager"]["image_url"], "name": team["name"], "nickname": team["managers"]["manager"]["nickname"]} for team in self.teams}
+        standings = [{"key": team["team_key"], "value": i+1} for i, team in enumerate(self.teams)] # More efficient to combine two loops?
         return teams_dict, standings
+
+    def get_players(self, player_keys):
+        return [
+            player 
+            for i in range(int(len(player_keys) / 25) + 1)
+            for player in self.get_response(
+                f"https://fantasysports.yahooapis.com/fantasy/v2/league/{self.league_key}/players;player_keys={','.join(player_keys[i*25:min((i+1)*25, len(player_keys))])};start={i*25}/stats"
+            )["league"]["players"]
+        ]
+
+    def get_top_n_players_by_position(self, n, position):
+        if position == "F":
+            position = "C,LW,RW"
+        return [
+            player 
+            for i in range(int(n/25)+1) 
+            for player in self.get_response(
+                f"https://fantasysports.yahooapis.com/fantasy/v2/league/{self.league_key}/players;sort=PTS;sort_type=season;position={position};count={n};start={i*25}/stats"
+            )["league"]["players"]
+        ]
+
+    def get_alternative_realities(self):
+        """
+        Returns:
+        alternative_reality_matrix (float[][]): Matrix of records if each team had another teams schedule
+        team_order (str[]): list of team_keys indicating the order of teams in the matrix
+        """
+        num_reg_weeks = self.playoff_start_week - self.league_start_week
+        weeks = ",".join([str(week) for week in range(self.league_start_week, self.playoff_start_week)])
+        url = f"https://fantasysports.yahooapis.com/fantasy/v2/league/{self.league_key}/scoreboard;week={weeks}/matchups"
+        matchups = self.get_response(url)["league"]["scoreboard"]["matchups"]
+        team_schedules = defaultdict(lambda: {"points": [], "opponent": []})
+        for matchup in matchups:
+            teams = matchup["teams"]
+            team_schedules[teams[0]["team_key"]]["points"].append(float(teams[0]["team_points"]["total"]))
+            team_schedules[teams[1]["team_key"]]["points"].append(float(teams[1]["team_points"]["total"]))
+            team_schedules[teams[0]["team_key"]]["opponent"].append({"key": teams[1]["team_key"], "points": float(teams[1]["team_points"]["total"])})
+            team_schedules[teams[1]["team_key"]]["opponent"].append({"key": teams[0]["team_key"], "points": float(teams[0]["team_points"]["total"])})
+        team_schedule_matrix = [[0 for _ in range(len(self.teams))] for _ in range(len(self.teams))]
+        for i, [team_a_key, team_a]  in enumerate(team_schedules.items()):
+            for j, team_b in enumerate(team_schedules.values()):
+                results = [0.5 if team_a_pts == team_opp["points"] else team_a_pts > team_opp["points"] for team_a_pts, team_opp in zip(team_a["points"], team_b["opponent"]) if team_opp["key"] != team_a_key]
+                percent = round(sum(results)/len(results), 3)
+                team_schedule_matrix[i][j] = percent
+        team_order = list(team_schedules.keys())
+        return team_schedule_matrix, team_order
+
+    def get_draft_busts_steals(self):
+        '''
+        Biggest draft busts/steals
+        '''
+        # Get draft results
+        url = f"{BASE_URL}/league/{self.league_key}/draftresults"
+        draft_results = self.get_response(url)["league"]["draft_results"]
+        draft_player_keys = [draft_result["player_key"] for draft_result in draft_results]
+        draft_players = self.get_players(draft_player_keys)
+        draft_players_by_pos = defaultdict(list)
+        positions_map = {"C": "F", "LW": "F", "RW": "F", "D": "D", "G": "G"}
+        for draft_player in draft_players:
+            draft_players_by_pos[positions_map[draft_player["primary_position"]]].append(draft_player)
+        # Get the top players by position
+        top_players_by_pos = {
+            "F": self.get_top_n_players_by_position(len(draft_players_by_pos["F"]), "F"), 
+            "D": self.get_top_n_players_by_position(len(draft_players_by_pos["D"]), "D"), 
+            "G": self.get_top_n_players_by_position(len(draft_players_by_pos["G"]), "G")
+        }
+        # Get differences between draft player and top player
+        diffs = []
+        for key in draft_players_by_pos:
+            for draft_player, top_player in zip(draft_players_by_pos[key], top_players_by_pos[key]):
+                diff = round(float(draft_player["player_points"]["total"]) - float(top_player["player_points"]["total"]), 1)
+                diffs.append((diff, draft_player))
+        smallest_diff = sorted(diffs, key=lambda x: x[0])
+        biggest_diff = sorted(diffs, reverse=True, key=lambda x: x[0])
+        draft_busts = [{"key": player["player_key"], "name": player["name"]["full"], "image": player["image_url"], "value": diff} for diff, player in smallest_diff[:5]]
+        draft_steals = [{"key": player["player_key"], "name": player["name"]["full"], "image": player["image_url"], "value": diff} for diff, player in biggest_diff[:5]]
+        return draft_busts, draft_steals
